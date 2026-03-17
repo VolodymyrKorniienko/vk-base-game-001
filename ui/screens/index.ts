f7e2670b65ea8aaedee3b8c730b25009ba407ba5
wayback_machine_downloader http://www.nomer.org/allukraina/ --to 20130117141850 <twokatanas@gmail.com>
2026-03-17 12:52:10 +0200

    Fix: remove GameMode type export

--- ui/screens/index.ts
+++ ui/screens/index.ts
@@ -1,4 +1,3 @@
 export { GameScreen } from './GameScreen';
 export { MenuScreen } from './MenuScreen';
 export { StageModeScreen } from './StageModeScreen';
-export type { GameMode } from './MenuScreen';
